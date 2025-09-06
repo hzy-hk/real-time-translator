dc0267c4527174526a3369bf72eb98ab9e8d70ea
hzy-hk <heziyi305@icloud.com>
2025-09-06 11:54:04 +0800

    Update overlay_mani.py
    difflib.SequenceMatcher for similarity measurement of texts

--- overlay_mani.py
+++ overlay_mani.py
@@ -1,8 +1,9 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 
+import difflib
 
 class SubtitleOverlay(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self, offset_x=0, offset_y=0):
         super().__init__()
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint |
                             QtCore.Qt.WindowStaysOnTopHint |
@@ -12,6 +13,8 @@ class SubtitleOverlay(QtWidgets.QWidget):
         # 屏幕大小
         self.screen = QtWidgets.QApplication.desktop().screenGeometry()
         self.setGeometry(self.screen)
+        self.offset_x = offset_x
+        self.offset_y = offset_y
         print('screen size (w, h)', self.screen.width(), self.screen.height())
         # 存放动态生成的 label
         self.active_labels = []
@@ -29,7 +32,7 @@ class SubtitleOverlay(QtWidgets.QWidget):
 
     def update_boxes_and_texts(self, boxes, text, img_h, img_w):
         """更新字幕文字"""
-
+        # print('Update boxes and texts')
         new_active = []
         for box, txt in zip(boxes, text):
             x1, y1, x2, y2 = box
@@ -38,10 +41,12 @@ class SubtitleOverlay(QtWidgets.QWidget):
 
             matched = None
             for old_box, old_txt, lbl in self.active_labels:
-                if txt == old_txt and self.iou(box, old_box) > 0.5:
+
+                if self.iou(box, old_box) > 0.5 and difflib.SequenceMatcher(None, txt, old_txt).ratio()>0.8:
+                    print('matched ', txt, old_txt, 'ratio: ', difflib.SequenceMatcher(None, txt, old_txt).ratio())
                     matched = (old_box, old_txt, lbl)
                     # 更新位置（避免抖动）
-                    lbl.setGeometry(x1, y1, x2 - x1, y2 - y1)
+                    lbl.setGeometry(x1+self.offset_x, y1+self.offset_y, x2 - x1, y2 - y1)
                     new_active.append((box, txt, lbl))
                     break
 
@@ -51,7 +56,7 @@ class SubtitleOverlay(QtWidgets.QWidget):
                 label = QtWidgets.QLabel(txt, self)
                 label.setStyleSheet("color: white; background-color: rgba(0,0,0,150); font-size: 18px;")
                 label.setAlignment(QtCore.Qt.AlignCenter)
-                label.setGeometry(int(x1), int(y1), int(w), int(h))
+                label.setGeometry(int(x1)+self.offset_x, int(y1)+self.offset_y, int(w), int(h))
                 label.setWordWrap(True)
                 label.show()
                 new_active.append((box, txt, label))
